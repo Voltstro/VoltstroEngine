b5c6d70718c994d00a0d7f64675b27cab1bf1300
Voltstro <me@voltstro.dev>
2020-06-24 18:37:54 +1000

    Replaced Magick.NET with StbImageSharp
    Handle it all in VoltstroEngine.Imaging

---
+++ src/VoltstroEngine.Imaging/Image.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using StbImageSharp;
+
+namespace VoltstroEngine.Imaging
+{
+	/// <summary>
+	/// Represents an image
+	/// </summary>
+	public class Image
+	{
+		/// <summary>
+		/// Creates and loads and new image
+		/// </summary>
+		/// <param name="fileLocation">Location of the image</param>
+		/// <param name="filp">Filp the image on load</param>
+		/// <exception cref="FileNotFoundException"></exception>
+		public Image(string fileLocation, bool filp)
+		{
+			if(!File.Exists(fileLocation))
+				throw new FileNotFoundException("Image doesn't exist!", fileLocation);
+
+			//Filp image on load, if we want to filp it
+			if(filp)
+				StbImage.stbi_set_flip_vertically_on_load(1);
+
+			//Open stream
+			FileStream imageStream = File.OpenRead(fileLocation);
+
+			//Create image
+			ImageResult image = ImageResult.FromStream(imageStream);
+			Data = image.Data;
+			Width = image.Width;
+			Height = image.Height;
+
+			//IDK if this was purposely done, but the enum number matches to the channels count of what the enum is
+			Channels = (int)image.SourceComp;
+		}
+
+		/// <summary>
+		/// The width of this image
+		/// </summary>
+		public readonly int Width;
+
+		/// <summary>
+		/// The height of this image
+		/// </summary>
+		public readonly int Height;
+
+		/// <summary>
+		/// How many channels does this image have?
+		/// </summary>
+		public readonly int Channels;
+
+		/// <summary>
+		/// The data of this image
+		/// </summary>
+		public readonly byte[] Data;
+	}
+}
--- src/VoltstroEngine/Platform/OpenGL/OpenGL2DTexture.cs
+++ src/VoltstroEngine/Platform/OpenGL/OpenGL2DTexture.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
-using ImageMagick;
 using OpenGL;
+using VoltstroEngine.Imaging;
 using VoltstroEngine.Rendering.Texture;
 
 namespace VoltstroEngine.Platform.OpenGL
@@ -13,16 +13,16 @@ namespace VoltstroEngine.Platform.OpenGL
 
 		public OpenGL2DTexture(string imagePath)
 		{
-			using MagickImage image = new MagickImage($"{Application.GameName}/{imagePath}");
+			Image image = new Image($"{Application.GameName}/{imagePath}", true);
 
 			InternalFormat internalFormat = 0;
 			PixelFormat dataFormat = 0;
-			if (image.ChannelCount == 4)
+			if (image.Channels == 4)
 			{
 				internalFormat = InternalFormat.Rgba8;
 				dataFormat = PixelFormat.Rgba;
 			}
-			else if (image.ChannelCount == 3)
+			else if (image.Channels == 3)
 			{
 				internalFormat = InternalFormat.Rgb8;
 				dataFormat = PixelFormat.Rgb;
@@ -33,17 +33,13 @@ namespace VoltstroEngine.Platform.OpenGL
 			width = (uint)image.Width;
 			height = (uint)image.Height;
 
-			image.Flip();
-
 			textureID = Gl.CreateTexture(TextureTarget.Texture2d);
 			Gl.TextureStorage2D(textureID, 1, internalFormat, image.Width, image.Height);
 
 			Gl.TextureParameteri(textureID, TextureParameterName.TextureMinFilter, Gl.LINEAR);
 			Gl.TextureParameteri(textureID, TextureParameterName.TextureMagFilter, Gl.NEAREST);
 
-			IPixelCollection<byte> pixels = image.GetPixels();
-			byte[] data = pixels.ToArray();
-			Gl.TextureSubImage2D(textureID, 0, 0, 0, image.Width, image.Height, dataFormat, PixelType.UnsignedByte, data);
+			Gl.TextureSubImage2D(textureID, 0, 0, 0, image.Width, image.Height, dataFormat, PixelType.UnsignedByte, image.Data);
 		}
 
 		~OpenGL2DTexture()
